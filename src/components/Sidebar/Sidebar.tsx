f4114ee6df17b4f7d9c41a93e7fe291b66127816
Gabriel Passos <kooujinproductions@gmail.com>
2022-07-12 00:35:39 -0300

    Applied responsiveness concepts

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from '../Avatar/Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
       
       <div className={styles.profile}>
-        <img src="https://github.com/Gabriel-Passoss.png" alt="" className={styles.avatar}/>
+        <Avatar src={"https://github.com/Gabriel-Passoss.png"}/>
 
         <strong>Gabriel dos Passos</strong>
         <span>Front-end Developer</span>
